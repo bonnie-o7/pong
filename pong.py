eb6b9154a14e24e3cb4aad7aa2b9e94164b9a63b
bonbon99 <58572137+bonbon99@users.noreply.github.com>
2020-06-03 19:43:25 -0400

    created server

--- pong.py
+++ pong.py
@@ -1,6 +1,17 @@
 import pygame
 import pygame.freetype
 import sys
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
 
 pygame.init()
 
@@ -102,3 +113,4 @@ while True:
     draw(paddle1, paddle2, ball)
 
     pygame.time.delay(int(1000/30))
+
