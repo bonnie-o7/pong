550bdb0e250275895b8166d44c55df290ea8f044
bonbon99 <58572137+bonbon99@users.noreply.github.com>
2020-06-11 19:25:05 -0400

    delete testing code

--- pong.py
+++ pong.py
@@ -3,16 +3,6 @@ import pygame.freetype
 import sys
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-print('Received', repr(data))
-
 pygame.init()
 
 # functions that moves paddles
--- server.py
+++ server.py
@@ -10,12 +10,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     """
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        pass
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 65432
